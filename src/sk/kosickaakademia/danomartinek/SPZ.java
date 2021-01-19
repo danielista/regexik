feab3b81f22cb81029f11cd9cbc546971c1af5d4
danielista <61905525+danielista@users.noreply.github.com>
2021-01-19 13:13:58 +0100

    i dont know neither KIKO nor others :D

--- src/sk/kosickaakademia/danomartinek/SPZ.java
+++ src/sk/kosickaakademia/danomartinek/SPZ.java
@@ -7,25 +7,18 @@ public class SPZ {
         return true;
     }
 
-    public boolean isValidOP (String text) {
+        public boolean isValidOP (String text) {
         if(text == null) return false;
-
-
         text = text.toUpperCase().trim();
-
         if (text.length() != 8) return false;
-
         for (int i =0; i<=1; i++){
             if(!Character.isLetter(text.charAt(i))) return false;
         }
-
         for (int i=2; i<=7; i++)
             if(!Character.isLetterOrDigit(text.charAt(i))) return false;
-
-
-
         return true;
     }
+
 
     public boolean isValidSPZ (String text) {
         if(text == null) return false;
