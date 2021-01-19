f86a74684f22f58c07bbbd01dde95bb2c630bb50
danielista <61905525+danielista@users.noreply.github.com>
2021-01-19 12:35:58 +0100

    updating isValidOP(String text); občiansky preukaz

--- src/sk/kosickaakademia/danomartinek/Main.java
+++ src/sk/kosickaakademia/danomartinek/Main.java
@@ -3,7 +3,7 @@ package sk.kosickaakademia.danomartinek;
 public class Main {
     public static void main(String[] args) {
         SPZ aa = new SPZ();
-        aa.isValidSPZ("KE 534iJ");
+       // aa.isValidSPZ("KE 534iJ");
         aa.isValidOP("SK2Y4784");
     }
 }
--- src/sk/kosickaakademia/danomartinek/SPZ.java
+++ src/sk/kosickaakademia/danomartinek/SPZ.java
@@ -2,25 +2,7 @@ package sk.kosickaakademia.danomartinek;
 
 public class SPZ {
 
-    public boolean isValidSPZ (String text) {
-        if(text == null) return false;
 
-
-        text = text.toUpperCase().trim();
-
-        if (text.length() != 7) return false;
-
-        for (int i =0; i<=1; i++){
-            if(!Character.isLetter(text.charAt(i))) return false;
-        }
-
-        for (int i=2; i<=6; i++)
-            if(!Character.isLetterOrDigit(text.charAt(i))) return false;
-
-
-
-        return true;
-    }
 
     public boolean isValidOP (String text) {
         if(text == null) return false;
