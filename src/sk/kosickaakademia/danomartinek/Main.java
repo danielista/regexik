1b43a64c21aac11fd07058077e0447a93fb78e08
danielista <61905525+danielista@users.noreply.github.com>
2021-01-19 12:29:43 +0100

    adding isValidOP(String text); občiansky preukaz

---
+++ src/sk/kosickaakademia/danomartinek/Main.java
@@ -0,0 +1,9 @@
+package sk.kosickaakademia.danomartinek;
+
+public class Main {
+    public static void main(String[] args) {
+        SPZ aa = new SPZ();
+        aa.isValidSPZ("KE 534iJ");
+        aa.isValidOP("SK2Y4784");
+    }
+}
