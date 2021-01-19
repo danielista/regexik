45105ea90f42651a81fce233cc1d3762cc4129c5
danielista <61905525+danielista@users.noreply.github.com>
2021-01-19 12:27:11 +0100

    SPZ isValidSPZ(String text);

---
+++ src/sk/kosickaakademia/danomartinek/SPZ.java
@@ -0,0 +1,25 @@
+package sk.kosickaakademia.danomartinek;
+
+public class SPZ {
+
+    public boolean isValidSPZ (String text) {
+        if(text == null) return false;
+
+
+        text = text.toUpperCase().trim();
+
+        if (text.length() != 7) return false;
+
+        for (int i =0; i<=1; i++){
+            if(!Character.isLetter(text.charAt(i))) return false;
+        }
+
+        for (int i=2; i<=6; i++)
+            if(!Character.isLetterOrDigit(text.charAt(i))) return false;
+
+
+
+        return true;
+    }
+
+}
